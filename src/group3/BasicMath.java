8e806f4f1e40cfdc266d0e26d593d872e4596a98
Malek <Malek@LAPTOP-MSSDAD36.fios-router.home>
2020-05-17 11:28:02 -0400

    Lets get to Work

---
+++ src/group3/BasicMath.java
@@ -0,0 +1,11 @@
+package group3;
+
+public class BasicMath {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Let's Do some Basic math codes");
+		// let's see what you got
+	}
+
+}
